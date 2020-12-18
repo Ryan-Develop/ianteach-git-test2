943cac5892f728e0e5a9fcab5638bce9b33d34f2
ianTeach <ianteach7@gmail.com>
2020-12-18 23:05:44 +0700

    menambahkan baris list (ul,li) di index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
 	<h1>Hello World!</h1>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ducimus, eum? Dolorum voluptates laborum, repellat. Illum quos sed commodi temporibus earum pariatur dolorum atque nulla error, impedit! Ipsam sunt, consectetur.</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 </body>
 </html>
